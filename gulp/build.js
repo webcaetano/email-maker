771d32bef4ac57aae601e428f5d5103a970127e5
unknown <dezaodoido@gmail.com>
2015-04-30 00:55:03 -0300

    fix some bug

--- gulp/build.js
+++ gulp/build.js
@@ -5,28 +5,31 @@ var cheerio = require('cheerio');
 var through = require('through2');
 var fs = require("fs");
 var imgAutoHost = require('gulp-image-autohost');
+var runSequence = require('run-sequence');
 
 var $ = require('gulp-load-plugins')({
 	pattern: ['gulp-*', 'main-bower-files', 'uglify-save-license', 'del']
 });
 
 module.exports = function(options) {
-	gulp.task('build',['clean','inject','other'],function () {
-		return gulp.src(options.tmp + '/serve/index.html')
-		.pipe(imgAutoHost())
-		.pipe(through.obj(function (file, enc, callback){
-			file.contents = new Buffer(cheerio.load(file.contents.toString())('.email-template').html());
-      		callback(null, file);
-		}))
-		.pipe($.minifyHtml())
-		.pipe(gulp.dest(options.dist + '/'))
-		.pipe($.size({ title: options.dist + '/', showFiles: true }));
+	gulp.task('build',function () {
+		runSequence('clean','other','inject',function(){
+			return gulp.src(options.tmp + '/serve/index.html')
+			.pipe(imgAutoHost())
+			.pipe(through.obj(function (file, enc, callback){
+				file.contents = new Buffer(cheerio.load(file.contents.toString())('.email-template').html());
+				callback(null, file);
+			}))
+			.pipe($.minifyHtml())
+			.pipe(gulp.dest(options.dist + '/'))
+			.pipe($.size({ title: options.dist + '/', showFiles: true }));
+		})
 	});
 
 	gulp.task('other', function () {
 		return gulp.src([
 			options.src + '/**/*',
-			'!' + options.src + '/**/*.{html,ico}',
+			'!' + options.src + '/**/*.{html,scss,ico}',
 		])
 		.pipe(gulp.dest(options.tmp + '/serve'));
 	});
